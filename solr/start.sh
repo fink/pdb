45be3255c2873fa510646c5249c184bc7feefb60
Andreas Gockel <gecko2@users.sourceforge.net>
2013-01-11 18:42:07 +0000

    Improve pdb logging and start script a bit =)

--- solr/start.sh
+++ solr/start.sh
@@ -1,27 +1,6 @@
 #!/bin/sh
 
-if [ -z "$JAVA_HOME" ]; then
-	for dir in \
-			/System/Library/Frameworks/JavaVM.framework/Versions/1.6*/Home \
-			/System/Library/Frameworks/JavaVM.framework/Versions/1.5*/Home \
-			/opt/jdk1.6* \
-			/opt/jdk1.5* \
-			/usr/java/jdk1.6* \
-			/usr/java/jdk1.5* \
-			/usr/lib/jvm/java-6-sun \
-			/usr/lib/jvm/java-1.6*-sun \
-			/usr/lib/jvm/java-1.5*-sun \
-			; do
-		if [ -x "$dir/bin/java" ]; then
-			export JAVA_HOME="$dir"
-			break;
-		fi
-	done
-fi
+BASEDIR=$(dirname "$0")
 
-LOGFILE=/dev/null
-MYDIR=`dirname "$0"`
-TOPDIR=`cd $MYDIR; pwd`
-
-cd "$TOPDIR"
-exec $JAVA_HOME/bin/java -Xms128m -Xmx512m -XX:MaxHeapFreeRatio=80 -XX:MinHeapFreeRatio=20 -Djava.net.preferIPv4Stack=true $SOLR_OPTS -jar $TOPDIR/start.jar >$LOGFILE 2>&1 &
+cd "$BASEDIR"
+( java -Xms128m -Xmx512m -XX:MaxHeapFreeRatio=80 -XX:MinHeapFreeRatio=20 -Djava.net.preferIPv4Stack=true -Djava.awt.headless=true $SOLR_OPTS -jar $BASEDIR/start.jar 2>&1 | logger -t solr ) &
